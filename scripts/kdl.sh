4acdb8857ceed29e361b65b666d0d374d8f0855f
streamregular <swirlixer@gmail.com>
2018-12-04 14:30:06 -0300

    Updated kdl.sh

--- scripts/kdl.sh
+++ scripts/kdl.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 if [ "$#" -eq 2 ]; then
+  echo ' '
   echo "Too many arguments. You're only supposed to provide one link."
+  echo ' '
+  exit 1
 fi
 
 if [ "$#" -eq 0 ]; then
+  echo ' '
   echo "Please provide one YouTube link."
+  echo ' '
+  exit 1
 fi
 
 echo -n "Name for the MV: "
